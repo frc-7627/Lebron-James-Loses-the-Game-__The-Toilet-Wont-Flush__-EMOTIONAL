9fb93c5c5d9df6e3878f6deb6a5b0750da217dfe
B0gos <admin@perhapsbot.dev>
2025-01-07 15:30:55 -0800

    Added javadocs to existing files

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,8 @@ import swervelib.math.Matter;
  *
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
+ * 
+ * @version 1.0
  */
 public final class Constants
 {
--- src/main/java/frc/robot/commands/vision/DoritoCommands.java
+++ src/main/java/frc/robot/commands/vision/DoritoCommands.java
@@ -4,16 +4,27 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.helpers.vision.DriveBaseRotationAdjust;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//import frc.robot.Constants.OperatorConstants;
-
+/**
+ * A set of Commands {@link edu.wpi.first.wpilibj2.command.Command}
+ * to be called either by automoous code or a button press
+ * 
+ * This class specifically contains Commands related to apriltag
+ * and object detections provided by wpilibpi
+ * 
+ * @version 1.0
+ */
 public class DoritoCommands {
     
-    /**  Limelight Control Commands */
     public DoritoCommands(){
     }
 
-    /** Updates Motor Speeds and limits from shuffleboard */
+    /**
+     *  Updates Motor Speeds and limits from shuffleboard
+     *  and makes all related Helper commands update as well 
+     * 
+     * @return void
+     * @version 1.0
+    */
     public void updateConstants() {
         System.out.println("[DoritoCommands] Shuffleboard Updated");
 
@@ -23,8 +34,10 @@ public class DoritoCommands {
 
     /**
      * Turns the Robot to face an April Tag
-     * @param drivebase Swerve Drivebase provided by YAGSL
-     * @return
+     * @param drivebase Swerve Drivebase {@link frc.robot.subsystems.swervedrive.SwerveSubsystem} provided by YAGSL
+     * @return edu.wpi.first.wpilibj2.command.Command
+     * 
+     * @version 1.0
      */
     public Command AdjustDriveBase(SwerveSubsystem drivebase) {
         return new DriveBaseRotationAdjust(drivebase);
