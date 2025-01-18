b0cf4eb2c8b225a5c5990198ad780087c73d4b6a
Elizabeth <lopez.elizabeth.alica@gmail.com>
2025-01-18 11:43:31 -0800

    Eggplant purple in WaitForCoral

--- src/main/java/frc/robot/commands/Endafector/WaitForCoral.java
+++ src/main/java/frc/robot/commands/Endafector/WaitForCoral.java
@@ -3,9 +3,12 @@ package frc.robot.commands.Endafector;
 import frc.robot.subsystems.arm.EndJoeBidenFactor;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Bluetooth;
 
 public class WaitForCoral extends Command {
     private EndJoeBidenFactor module;
+    Bluetooth bluetooth = new Bluetooth();
+    EndJoeBidenFactor joe = new EndJoeBidenFactor();
 
     public WaitForCoral(EndJoeBidenFactor module) {
         this.module = module;
@@ -17,8 +20,15 @@ public class WaitForCoral extends Command {
         module.stop();
     }
 
+    @Override
+    public void execute(){
+        if (joe.CoralIn()){
+        bluetooth.eggPlant();
+    }
+    }
+
     @Override 
     public boolean isFinished() {
-        return (module.CoralIn());
+        return false;
     }
 }
