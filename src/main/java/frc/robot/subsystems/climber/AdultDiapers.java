fa4b4bdc798a75e851cb664d99c80473a5eb2163
B0gos <admin@perhapsbot.dev>
2025-02-19 15:27:39 -0800

    Added motor config to Climber Subsystem

--- src/main/java/frc/robot/subsystems/climber/AdultDiapers.java
+++ src/main/java/frc/robot/subsystems/climber/AdultDiapers.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.climber;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -16,13 +18,42 @@ public class AdultDiapers extends SubsystemBase {
 
     // Instance variables
     private static double movementSpeed = 0.5;  
-    private static double slowMoveSpeed = 0.5 * movementSpeed;  
+    private static double slowMoveSpeed = 0.5 * movementSpeed; 
+    
+    private static double rampUpPeriod = 0.5;
+
+    private static double currentLimit = 40;
 
     // Define Motor IDs
     final TalonFX m_talonFX = new TalonFX(50);
 
     /** Initiallizes the Climber Subsystem */
     public AdultDiapers() {
+        // in init function
+        var talonFXConfig = new TalonFXConfiguration();
+        
+        // Current limit
+        talonFXConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
+        talonFXConfig.CurrentLimits.withStatorCurrentLimit(currentLimit);
+
+        // Speed limit
+        talonFXConfig.MotorOutput.withPeakForwardDutyCycle(movementSpeed);
+        talonFXConfig.MotorOutput.withPeakReverseDutyCycle(movementSpeed);
+
+        talonFXConfig.MotorOutput.withNeutralMode(NeutralModeValue.Brake); // Set to brake mode
+
+        // Ramp Up speed
+        talonFXConfig.OpenLoopRamps.withDutyCycleOpenLoopRampPeriod(rampUpPeriod);
+        talonFXConfig.ClosedLoopRamps.withDutyCycleClosedLoopRampPeriod(rampUpPeriod);
+
+        // Music stuff
+        talonFXConfig.Audio.withBeepOnBoot(false);
+        talonFXConfig.Audio.withBeepOnConfig(false);
+        talonFXConfig.Audio.withAllowMusicDurDisable(true);
+
+        m_talonFX.getConfigurator().apply(talonFXConfig); // Apply Motor Config
+
+
     }
 
     /**
