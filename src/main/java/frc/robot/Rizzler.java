cdd92bebb4ad5a2f73a5054ae57ac9581d8f02d1
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-06 16:39:35 -0800

    Lint

--- src/main/java/frc/robot/Rizzler.java
+++ src/main/java/frc/robot/Rizzler.java
@@ -1,30 +1,36 @@
 package frc.robot;
 
 
-   /**
-     * A simple startup Banner to run when the code starts
-     * Additionally runs checks and presents warnings
-     * to the console at startup
-     * 
-     * @see System.out.println
-     */
+/**
+ * A simple startup Banner to run when the code starts Additionally runs checks and presents
+ * warnings to the console at startup
+ * 
+ * @see System.out.println
+ */
 public class Rizzler {
 
     public static void rizz() {
-        /*System.out.println("  ___   _           _     _   _                                       \r\n" + //
-                                " |_ _| | | ___  ___| |_  | |_| |__   ___    __ _  __ _ _ __ ___   ___ \r\n" + //
-                                "  | |  | |/ _ \\/ __| __| | __| '_ \\ / _ \\  / _` |/ _` | '_ ` _ \\ / _ \\\r\n" + //
-                                "  | |  | | (_) \\__ \\ |_  | |_| | | |  __/ | (_| | (_| | | | | | |  __/\r\n" + //
-                                " |___| |_|\\___/|___/\\__|  \\__|_| |_|\\___|  \\__, |\\__,_|_| |_| |_|\\___|\r\n" + //
-                                "                                           |___/                      ");*/
+        /*
+         * System.out.
+         * println("  ___   _           _     _   _                                       \r\n" + //
+         * " |_ _| | | ___  ___| |_  | |_| |__   ___    __ _  __ _ _ __ ___   ___ \r\n" + //
+         * "  | |  | |/ _ \\/ __| __| | __| '_ \\ / _ \\  / _` |/ _` | '_ ` _ \\ / _ \\\r\n" + //
+         * "  | |  | | (_) \\__ \\ |_  | |_| | | |  __/ | (_| | (_| | | | | | |  __/\r\n" + //
+         * " |___| |_|\\___/|___/\\__|  \\__|_| |_|\\___|  \\__, |\\__,_|_| |_| |_|\\___|\r\n" + //
+         * "                                           |___/                      ");
+         */
 
         System.out.println(
-                        "___   _     ___  ____ _____   _____ _   _ _____    ____    _    __  __ _____ \r\n" + //
-                        "|_ _| | |   / _ \\/ ___|_   _| |_   _| | | | ____|  / ___|  / \\  |  \\/  | ____|\r\n" + //
-                        "| |  | |  | | | \\___ \\ | |     | | | |_| |  _|   | |  _  / _ \\ | |\\/| |  _|  \r\n" + //
-                       " | |  | |__| |_| |___) || |     | | |  _  | |___  | |_| |/ ___ \\| |  | | |___ \r\n" + //
+                "___   _     ___  ____ _____   _____ _   _ _____    ____    _    __  __ _____ \r\n"
+                        + //
+                        "|_ _| | |   / _ \\/ ___|_   _| |_   _| | | | ____|  / ___|  / \\  |  \\/  | ____|\r\n"
+                        + //
+                        "| |  | |  | | | \\___ \\ | |     | | | |_| |  _|   | |  _  / _ \\ | |\\/| |  _|  \r\n"
+                        + //
+                        " | |  | |__| |_| |___) || |     | | |  _  | |___  | |_| |/ ___ \\| |  | | |___ \r\n"
+                        + //
                         "|___| |_____\\___/|____/ |_|     |_| |_| |_|_____|  \\____/_/   \\_\\_|  |_|_____|");
-        
+
         griddyemail();
         check_skibbidi();
     }
@@ -40,11 +46,10 @@ public class Rizzler {
 
     }
 
-	// This was written on a laptop older than me
+    // This was written on a laptop older than me
 
     /**
-     * Checks if coach controller is enabled in constants or not
-     * Prints a warning message if it is
+     * Checks if coach controller is enabled in constants or not Prints a warning message if it is
      * 
      * @see Constants.skiibi_mode
      * @return boolean - whether coach controller is enabled or not
@@ -52,17 +57,16 @@ public class Rizzler {
      * @version 1.0
      */
     public static boolean check_skibbidi() {
-        if(Constants.skibbidi_mode) {
+        if (Constants.skibbidi_mode) {
             System.out.println("Coach controller enabled!\r\n" + //
-                                "________________________________________ \r\n" + //
-                                "|                                       |\r\n" + //
-                                "| !!!DO NOT USE THIS CODE IN A MATCH!!! |\r\n" + //
-                                "|_______________________________________|\r\n" + //
-                                "                               \r\n" + //
-                                "Robot may attempt to rizz up the ops");
+                    "________________________________________ \r\n" + //
+                    "|                                       |\r\n" + //
+                    "| !!!DO NOT USE THIS CODE IN A MATCH!!! |\r\n" + //
+                    "|_______________________________________|\r\n" + //
+                    "                               \r\n" + //
+                    "Robot may attempt to rizz up the ops");
             return true;
-        }
-        else {
+        } else {
             System.out.println("Coach controller disabled");
             return false;
         }
--- src/main/java/frc/robot/subsystems/climber/AdultDiapers.java
+++ src/main/java/frc/robot/subsystems/climber/AdultDiapers.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-/** 
+/**
  * Climber Subsystem
  * 
  * Uses a singe TalonFX motor controller
@@ -23,9 +23,9 @@ import frc.robot.Constants;
 public class AdultDiapers extends SubsystemBase {
 
     // Instance variables
-    private static double movementSpeed = 0.5;  
-    private static double slowMoveSpeed = 0.5 * movementSpeed; 
-    
+    private static double movementSpeed = 0.5;
+    private static double slowMoveSpeed = 0.5 * movementSpeed;
+
     private static double rampUpPeriod = 0.5;
 
     private static double currentLimit = 40;
@@ -37,7 +37,7 @@ public class AdultDiapers extends SubsystemBase {
     public AdultDiapers() {
         // in init function
         var talonFXConfig = new TalonFXConfiguration();
-        
+
         // Current limit
         talonFXConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
         talonFXConfig.CurrentLimits.withStatorCurrentLimit(currentLimit);
@@ -61,12 +61,11 @@ public class AdultDiapers extends SubsystemBase {
 
 
     }
-    
+
 
     /**
-     * Moves the climber up
-     * at a speed specified in the instance variable
-     * MoveSpeed for general control
+     * Moves the climber up at a speed specified in the instance variable MoveSpeed for general
+     * control
      * 
      * @return void
      * @version 1.0
@@ -76,9 +75,8 @@ public class AdultDiapers extends SubsystemBase {
     }
 
     /**
-     * Moves the climber down
-     * at a speed specified in the instance variable
-     * MoveSpeed for general control
+     * Moves the climber down at a speed specified in the instance variable MoveSpeed for general
+     * control
      * 
      * @return void
      * @version 1.0
@@ -88,9 +86,8 @@ public class AdultDiapers extends SubsystemBase {
     }
 
     /**
-     * Slowly moves the climber up
-     * at a speed specified in the instance variable
-     * slowMoveSpeed for fine control
+     * Slowly moves the climber up at a speed specified in the instance variable slowMoveSpeed for
+     * fine control
      * 
      * @return void
      * @version 1.0
@@ -100,9 +97,8 @@ public class AdultDiapers extends SubsystemBase {
     }
 
     /**
-     * Slowly moves the climber down
-     * at a speed specified in the instance variable
-     * slowMoveSpeed for fine control
+     * Slowly moves the climber down at a speed specified in the instance variable slowMoveSpeed for
+     * fine control
      * 
      * @return void
      * @version 1.0
@@ -153,17 +149,16 @@ public class AdultDiapers extends SubsystemBase {
 
 
     /**
-    * Similates an issue with the current subsystem
-    * Only works if skibbidi-mode is enabled
-    *
-    * Slows climber motors down to nothing
-    * 
-    * @return void
-    * @version 1.0
-    */
+     * Similates an issue with the current subsystem Only works if skibbidi-mode is enabled
+     *
+     * Slows climber motors down to nothing
+     * 
+     * @return void
+     * @version 1.0
+     */
     public void simulateFault() {
         // Check for Coach Mode
-        if(!Constants.skibbidi_mode) {
+        if (!Constants.skibbidi_mode) {
             System.out.println("[Endefector] Coach Controller Disabled!");
             return; // Do not finish running method
         }
@@ -176,74 +171,89 @@ public class AdultDiapers extends SubsystemBase {
 
 
     /**
-     * Gets all fields and getter methods in this class and 
-     * places their values from shuffleboard
+     * Gets all fields and getter methods in this class and places their values from shuffleboard
      * 
      * @return void
      * @version 1.0
      */
     public void pushData() {
-        String shuffleboardName = this.getClass().getCanonicalName().replace('.', '/').substring(10);
+        String shuffleboardName =
+                this.getClass().getCanonicalName().replace('.', '/').substring(10);
 
         Method[] methods = this.getClass().getDeclaredMethods();
-        for (Method method:methods)
-        {
-            if(method.getName().substring(0, 3).equals("get")) {
+        for (Method method : methods) {
+            if (method.getName().substring(0, 3).equals("get")) {
                 try {
                     Object value = method.invoke(this);
-                    if(value == null) value = 0.0; // Set to zero in case we can't run method
-                    SmartDashboard.putNumber(shuffleboardName + "/" + method.getName().substring(3), Double.parseDouble(value.toString()));
-                    //System.out.println(method.getName().substring(3) + " value:" + Double.parseDouble(value.toString()));
-                } catch(IllegalAccessException e) {
-                    System.out.println("[" + shuffleboardName + "] Somthing went wrong getting Shuffleboard data for: " + method.getName());
-                } catch(InvocationTargetException e) {
-                    System.out.println("[" + shuffleboardName + "] Somthing went wrong getting Shuffleboard data for: " + method.getName());
+                    if (value == null)
+                        value = 0.0; // Set to zero in case we can't run method
+                    SmartDashboard.putNumber(shuffleboardName + "/" + method.getName().substring(3),
+                            Double.parseDouble(value.toString()));
+                    // System.out.println(method.getName().substring(3) + " value:" +
+                    // Double.parseDouble(value.toString()));
+                } catch (IllegalAccessException e) {
+                    System.out.println("[" + shuffleboardName
+                            + "] Somthing went wrong getting Shuffleboard data for: "
+                            + method.getName());
+                } catch (InvocationTargetException e) {
+                    System.out.println("[" + shuffleboardName
+                            + "] Somthing went wrong getting Shuffleboard data for: "
+                            + method.getName());
                 }
             }
         }
         Field[] declaredFields = this.getClass().getDeclaredFields();
-        for (Field field : declaredFields) {  
+        for (Field field : declaredFields) {
             if (field.getType().isPrimitive()) {
                 try {
-                    SmartDashboard.putNumber(shuffleboardName + "/" + field.getName(), field.getDouble(this.getClass()));
-                    //System.out.println(field.getName() + " value: " + field.getDouble(this.getClass()));
-                } catch(IllegalAccessException e) {
-                    System.out.println("[" + shuffleboardName + "] Somthing went wrong getting Shuffleboard data for: " + field.getName());
+                    SmartDashboard.putNumber(shuffleboardName + "/" + field.getName(),
+                            field.getDouble(this.getClass()));
+                    // System.out.println(field.getName() + " value: " +
+                    // field.getDouble(this.getClass()));
+                } catch (IllegalAccessException e) {
+                    System.out.println("[" + shuffleboardName
+                            + "] Somthing went wrong getting Shuffleboard data for: "
+                            + field.getName());
                 }
             }
-        }   
+        }
     }
 
     /**
-     * Gets all feilds in this class and updates their values from shuffleboard
-     * !! Make sure to run pushData first !!
+     * Gets all feilds in this class and updates their values from shuffleboard !! Make sure to run
+     * pushData first !!
      * 
      * @return void
      * @version 1.0
      */
     public void pullData() {
-        String shuffleboardName = this.getClass().getCanonicalName().replace('.', '/').substring(10);
+        String shuffleboardName =
+                this.getClass().getCanonicalName().replace('.', '/').substring(10);
         Field[] declaredFields = this.getClass().getDeclaredFields();
         for (Field field : declaredFields) {
             if (field.getType().isPrimitive() && !Modifier.isStatic(field.getModifiers())) {
                 try {
-                    field.setDouble(this, SmartDashboard.getNumber(shuffleboardName + "/const/" + field.getName(), field.getDouble(this.getClass())));
-                    System.out.println(field.getName() + " set " + field.getDouble(this.getClass()));
-                } catch(IllegalAccessException e) {
-                    System.out.println("[" + shuffleboardName + "] Somthing went wrong getting Shuffleboard data for: " + field.getName());
+                    field.setDouble(this,
+                            SmartDashboard.getNumber(shuffleboardName + "/const/" + field.getName(),
+                                    field.getDouble(this.getClass())));
+                    System.out
+                            .println(field.getName() + " set " + field.getDouble(this.getClass()));
+                } catch (IllegalAccessException e) {
+                    System.out.println("[" + shuffleboardName
+                            + "] Somthing went wrong getting Shuffleboard data for: "
+                            + field.getName());
                 }
             }
-        }   
+        }
     }
 
     /** Run once every periodic call */
-    /** 
-     *  Run once every periodic call as
-     *  long as the Command is running 
+    /**
+     * Run once every periodic call as long as the Command is running
      */
     @Override
     public void periodic() {
-        if(Constants.verbose_shuffleboard_logging) {
+        if (Constants.verbose_shuffleboard_logging) {
             pushData();
             pullData();
         }
